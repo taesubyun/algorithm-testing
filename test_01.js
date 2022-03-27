880abd41d5be5d81fe0ed5f7923ca2f630da1d50
Taesub Yun <taesubyun@icloud.com>
2022-03-27 18:22:09 +0900

    readme 워딩 수정

--- test_01.js
+++ test_01.js
@@ -4,16 +4,16 @@
 // 📌 풀이
 
 function solution(id_list, report, k) {
+    // 간단하게
+    // report를 for문을 돌려서, 신고자 -> 신고대상 쪼개는 로직이 필요할 것 같다.
+    // 그 과정에서, 신고대상이 k번 이상 나오는지 체크.
+    // K번 넘은 신고대상이 추려진다.
     const answer = [];
 
-    // 쉬운 로직
-    // report를 for문을 돌려서, 신고자->신고대상 쪼개는 로직이 필요할 것 같다.
-    // 그 과정에서, 신고대상이 k번 이상 나오는지 체크
-    // K번 넘은 신고대상이 추려진다.
-
     const reporteeCounter = {};
+    // 신고 당한 횟수
     const blockedReportees = [];
-
+    // 신고 당한 유저
     let uniqueReport = Array.from(new Set(report));
 
     for (let i = 0; i < uniqueReport.length; i++) {
@@ -33,7 +33,9 @@ function solution(id_list, report, k) {
     // report 루프를 돌린다.
     // k번을 넘은 애를 알고 있다.
     // 루프를 돌리면서, 어떤 유저가 k번 넘은 애를 신고하고 있으면, 걔를 counter에 넣어준다.
+
     const mailedCounter = {};
+
     for (let i = 0; i < uniqueReport.length; i++) {
         const [reporter, reportee] = uniqueReport[i].split(' ');
         if (blockedReportees.includes(reportee)) {
