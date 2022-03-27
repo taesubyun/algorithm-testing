3d9ee51b0cdb0e72cb071feafa9a965c5c1aee0e
Taesub Yun <taesubyun@icloud.com>
2022-03-27 18:17:58 +0900

    로직 작성

--- test_01.js
+++ test_01.js
@@ -5,5 +5,57 @@
 
 function solution(id_list, report, k) {
     const answer = [];
+
+    // 쉬운 로직
+    // report를 for문을 돌려서, 신고자->신고대상 쪼개는 로직이 필요할 것 같다.
+    // 그 과정에서, 신고대상이 k번 이상 나오는지 체크
+    // K번 넘은 신고대상이 추려진다.
+
+    const reporteeCounter = {};
+    const blockedReportees = [];
+
+    let uniqueReport = Array.from(new Set(report));
+
+    for (let i = 0; i < uniqueReport.length; i++) {
+        const [reporter, reportee] = uniqueReport[i].split(' ');
+
+        if (reportee in reporteeCounter) {
+            reporteeCounter[reportee] += 1;
+        } else {
+            reporteeCounter[reportee] = 1;
+        }
+
+        if (reporteeCounter[reportee] >= k) {
+            blockedReportees.push(reportee);
+        }
+    }
+
+    // report 루프를 돌린다.
+    // k번을 넘은 애를 알고 있다.
+    // 루프를 돌리면서, 어떤 유저가 k번 넘은 애를 신고하고 있으면, 걔를 counter에 넣어준다.
+    const mailedCounter = {};
+    for (let i = 0; i < uniqueReport.length; i++) {
+        const [reporter, reportee] = uniqueReport[i].split(' ');
+        if (blockedReportees.includes(reportee)) {
+            if (reporter in mailedCounter) {
+                mailedCounter[reporter] += 1;
+            } else {
+                mailedCounter[reporter] = 1;
+            }
+        }
+    }
+
+    // formatting
+    // { muzi: 2, frodo: 3 }
+    // [2, 1, 1, 1]
+
+    for (let i = 0; i < id_list.length; i++) {
+        if (id_list[i] in mailedCounter) {
+            answer.push(mailedCounter[id_list[i]]);
+        } else {
+            answer.push(0);
+        }
+    }
+
     return answer;
 }
