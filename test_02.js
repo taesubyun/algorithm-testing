45155841c4796cc105da2b2e4471d9939df2341e
Taesub Yun <taesubyun@icloud.com>
2022-03-29 17:22:36 +0900

    문자열을 배열에 담는 numbers 변수 생성

--- test_02.js
+++ test_02.js
@@ -2,3 +2,19 @@
 // - 숫자 문자열과 영단어
 
 // 📌 풀이
+function solution(s) {
+    let numbers = [
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+    ];
+    let answer = s;
+    return result;
+}
